ed596bf1ab733ff64a633676669d0c24c5a2b19d
ACER <musikkunyt@gmail.com>
2020-05-12 21:31:41 +0700

    Coba Rest Api di Modul Sukses

--- app/src/main/java/com/example/pantaucorona/GithubFragmen.kt
+++ app/src/main/java/com/example/pantaucorona/GithubFragmen.kt
@@ -1,12 +1,23 @@
 package com.example.pantaucorona
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.Nullable
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.pantaucorona.data.GithubService
+import com.example.pantaucorona.data.apiRequest
+import com.example.pantaucorona.data.httpClient
+import com.example.pantaucorona.util.dismissLoading
+import com.example.pantaucorona.util.showLoading
+import com.example.pantaucorona.util.tampilToast
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.fragment_github.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class GithubFragmen : Fragment() {
@@ -14,18 +25,64 @@ class GithubFragmen : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_github,
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {return inflater.inflate(R.layout.fragment_github,
             container, false)
     }
-    override fun onViewCreated(
-        view: View,
-        @Nullable savedInstanceState: Bundle?
-    ) {
+    override fun onViewCreated(view: View, @Nullable savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        callApiGetGithubUser()
+    }
+
+    private fun callApiGetGithubUser() {
+        showLoading(context!!, swipeRefreshLayout)
+        val httpClient = httpClient()
+        val apiRequest = apiRequest<GithubService>(httpClient)
+        val call = apiRequest.getUsers()
+        call.enqueue(object : Callback<List<GithubUserItem>> {
+            override fun onFailure(call: Call<List<GithubUserItem>>, t: Throwable) {
+                dismissLoading(swipeRefreshLayout)
+            }
+            override fun onResponse(call: Call<List<GithubUserItem>>, response:
+            Response<List<GithubUserItem>>
+            ) {
+                dismissLoading(swipeRefreshLayout)
+                when {
+                    response.isSuccessful ->
+                        when {
+                            response.body()?.size != 0 ->
+                                tampilGithubUser(response.body()!!)
+                            else -> {
+                                tampilToast(
+                                    context!!,
+                                    "Berhasil"
+                                )
+                            }
+                        }
+                    else -> {
+                        tampilToast(
+                            context!!,
+                            "Gagal"
+                        )
+                    }
+                }
+            }
+        })
+    }
+
+    private fun tampilGithubUser(githubUsers: List<GithubUserItem>) {
+        listGithubUser.layoutManager = LinearLayoutManager(context)
+        listGithubUser.adapter = GithubUserAdapter(context!!, githubUsers) {
+            val githubUser = it
+            tampilToast(
+                context!!,
+                githubUser.login
+            )
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        this.clearFindViewByIdCache()
     }
 
 }
